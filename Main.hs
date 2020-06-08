84abf9cb1105f5c8ee730186475f8077a3942bea
daniel <daniel@bogusemailserver.com>
2020-06-08 22:12:20 +0200

    benchmarks

--- Main.hs
+++ Main.hs
@@ -6,10 +6,10 @@ import Streaming
 import qualified Streaming.Prelude as S
 import qualified Data.ByteString.Streaming as BS
 
-
 readProust :: IO B.ByteString
 readProust = B.readFile "./2650-0.txt"   
 
+-- | take a big preexisting bytestring, generate a stream of chunks of size chunkSize
 stream1 :: Int -> B.ByteString -> Stream (Of B.ByteString) IO ()
 stream1 chunkSize = go
     where
@@ -21,6 +21,7 @@ stream1 chunkSize = go
                 do S.yield chunk
                    go rest
 
+-- | take a big preexisting bytestring, generate a stream of chunks of size chunkSize
 stream2 :: Int -> B.ByteString -> BS.ByteString IO ()
 stream2 chunkSize = go
     where
@@ -31,13 +32,22 @@ stream2 chunkSize = go
             else 
                 do BS.chunk chunk
                    go rest
+
+-- | accumulate the length of a stream of strict bytestring chunks
+benchmark1 :: B.ByteString -> Benchmark
+benchmark1 = nfIO . S.fold_ (\acc b -> acc + B.length b) 0 id . stream1 4
+
+-- | calculate the length of a streamed ByteString
+benchmark1 :: B.ByteString -> Benchmark
+benchmark1 = nfIO . BS.length_ . stream2 4 
+
 main :: IO ()
 main = do
-    swann <- readProust 
-    print $ B.length swann
+    let batchSize = 4
     defaultMain [
                 bgroup "foo" [
+                      env readProust $ bench "Stream len" . benchmark1
+                    , env readProust $ bench "ByteString len " . benchmark2
             ]
         ]
-    return ()
 
